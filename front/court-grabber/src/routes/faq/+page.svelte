c4d3d466a75796629d80da11eaa7a1dcc43a34d4
Justin Garner <charlottewebsolutions1@gmail.com>
2023-11-08 23:53:28 -0500

    FAQ page complete

---
+++ front/court-grabber/src/routes/faq/+page.svelte
@@ -0,0 +1,77 @@
+<script>
+    import SidebarNav from '$lib/components/SidebarNav.svelte';
+</script>
+
+<SidebarNav/>
+
+<div class="p-4 sm:ml-64 pt-20 bg-white h-screen">
+    <section class="bg-white">
+        
+        <div class="py-6 px-4 mx-auto max-w-screen-xl sm:py-4 lg:px-6">
+            <a href="/help" class="text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2">Back</a>
+            <h2 class="mt-10 mb-8 text-4xl tracking-tight font-extrabold text-gray-900">Frequently asked questions</h2>
+            <div class="grid pt-8 text-left border-t border-gray-200 md:gap-16 md:grid-cols-2">
+                <div>
+                    <div class="mb-10">
+                        <h3 class="flex items-center mb-4 text-lg font-medium text-gray-900">
+                            <svg class="flex-shrink-0 mr-2 w-5 h-5 text-gray-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd"></path></svg>
+                            How does the court booking process work?
+                        </h3>
+                        <p class="text-gray-500">Our app allows you to easily search for available courts at your favorite parks, select your preferred date/time, and instantly book the court. Our technology automates the reservation process so you don't have attempt to book courts before others on the park and rec site.</p>
+                    </div>
+                    <div class="mb-10">                        
+                        <h3 class="flex items-center mb-4 text-lg font-medium text-gray-900">
+                            <svg class="flex-shrink-0 mr-2 w-5 h-5 text-gray-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd"></path></svg>
+                            What parks and courts do you currently support for booking?
+                        </h3>
+                        <p class="text-gray-500">Right now we support booking for the tennis courts at Latta Park, Freedom Park, Veterans Park, and Jeff Adams Tennis Center. We're rapidly expanding to support more parks soon.</p>
+                    </div>
+                    <div class="mb-10">
+                        <h3 class="flex items-center mb-4 text-lg font-medium text-gray-900">
+                            <svg class="flex-shrink-0 mr-2 w-5 h-5 text-gray-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd"></path></svg>
+                            How far in advance can I book a court through your service?
+                        </h3>
+                        <p class="text-gray-500">You can book a court up to 14 days in advance to ensure you get the court and time you want. For last-minute bookings, courts can be booked as soon as 5 mins before the start time.</p>
+                    </div>
+                    <div class="mb-10">
+                        <h3 class="flex items-center mb-4 text-lg font-medium text-gray-900">
+                            <svg class="flex-shrink-0 mr-2 w-5 h-5 text-gray-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd"></path></svg>
+                            Is there a limit to how many court bookings I can make per day or week?
+                        </h3>
+                        <p class="text-gray-500">No, there's no limit! Book as many courts as you'd like. We're all about defying limitations.</p>
+                    </div>
+                </div>
+                <div>
+                    <div class="mb-10">
+                        <h3 class="flex items-center mb-4 text-lg font-medium text-gray-900">
+                            <svg class="flex-shrink-0 mr-2 w-5 h-5 text-gray-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd"></path></svg>
+                            How much does it cost to use the court booking service?
+                        </h3>
+                        <p class="text-gray-500">We offer multiple subscription plans to fit your specific needs. Weather you are a recreational or competitive player or even a group organizer we got a very affordable plan thats perfect for you! </p>
+                    </div>
+                    <div class="mb-10">
+                        <h3 class="flex items-center mb-4 text-lg font-medium text-gray-900">
+                            <svg class="flex-shrink-0 mr-2 w-5 h-5 text-gray-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd"></path></svg>
+                            Can I use the service to book courts for group play?
+                        </h3>
+                        <p class="text-gray-500">Absolutely! We've got a plan perfect for any tennis group that you can use to book as many courts as you need for as long as you would like.</p>
+                    </div>
+                    <div class="mb-10">
+                        <h3 class="flex items-center mb-4 text-lg font-medium text-gray-900">
+                            <svg class="flex-shrink-0 mr-2 w-5 h-5 text-gray-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd"></path></svg>
+                            How do I access my upcoming court bookings in the app?
+                        </h3>
+                        <p class="text-gray-500">In your dashboard, you'll see a list of your all your bookings including upcoming bookings with details on location, date, and time. You can also opt into receive email and text reminders for your bookings.</p>
+                    </div>
+                    <div class="mb-10">
+                        <h3 class="flex items-center mb-4 text-lg font-medium text-gray-900">
+                            <svg class="flex-shrink-0 mr-2 w-5 h-5 text-gray-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd"></path></svg>
+                            Do you offer any sweet perks for frequent court bookings through the service?
+                        </h3>
+                        <p class="text-gray-500">Yes! We are currently running a program where when you book 5 courts using court booker, you can get a free booking on us. Check your dashboard to see how close you are to a free booking!</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
